747a9ad80c0f26b03c54fc03db533070e6c25167
pupubird <rainchai4240@gmail.com>
2020-09-24 20:59:08 +0800

    Add srt to txt

---
+++ app.py
@@ -0,0 +1,41 @@
+def output_txt(file_name):
+    """Output srt file into txt with certain format
+
+    Parameters:
+        file_name (string): the file_name / path to the srt file
+
+    Effect:
+        Generate a processed txt file with the same name as the file_name
+
+    Below is the pattern for the srt file:
+    1
+    00:00:01,040 --> 00:00:08,320
+    and also i think we all know mrs is a
+
+    2
+    00:00:04,319 --> 00:00:11,040
+    big full of elites especially from the
+    """
+    with open(file_name, "r", encoding="utf-8") as f:
+        output = []
+        lines = f.readlines()
+
+        # Clean all the whitespace line
+        for index, line in enumerate(lines):
+            if line == "\n":
+                del lines[index]
+
+        # Get text with steps
+        for line in lines[2::3]:
+            line = line.replace("\n", " ")
+            output.append(line)
+
+        # Save file
+        with open(file_name.replace("srt", 'txt'), "w") as g:
+            g.writelines(output)
+
+
+# Example usage
+output_txt("captions.srt")
+output_txt("captions (1).srt")
+output_txt("captions (2).srt")
